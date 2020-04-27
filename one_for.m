a9cace9b648e575266b3026ba0d7cb1fa115d621
Tim <djhungtim@gmail.com>
2020-04-27 16:52:46 +0800

    think about interplote

--- one_for.m
+++ one_for.m
@@ -1,4 +1,4 @@
-%% This code is for modis RefSB % GEO location plot
+                %% This code is for modis RefSB % GEO location plot
 
 %% load RSB data
 
@@ -12,12 +12,12 @@ opengl('save', 'hardware')
 tic % start time tracker
 
 
-EV_RSB_dir = dir('D:\SPL\dataAnalysis\springData\MOD021KM*.hdf');
+EV_RSB_dir = dir('D:\tim_spl\springData\MOD021KM*.hdf');
 
 
-RSB_NUM = length(EV_RSB_dir); % calculate length of RSB data
+RSB_NUM = 50;%length(EV_RSB_dir); % calculate length of RSB data
 
-EV_GEO_dir = dir('D:\SPL\dataAnalysis\springDataGeo\MOD03*.hdf');
+EV_GEO_dir = dir('D:\tim_spl\springDataGeo\MOD03*.hdf');
 
 %import both Latitude and Longitude
 GEO_NUM = 1;%length(EV_GEO_dir); % calculate length of GEO data
@@ -49,21 +49,34 @@ for idx = 1:RSB_NUM
     Latitude(Latitude == -999) = 0;
     Latitude = double(Latitude);
     
-%     [xq,yq] = meshgrid(min(Longitude(:,1)):.2:max(Longitude(:,1)), min(Latitude(:,1)):.2:max(Latitude(:,1)));
-%     z1 = griddata(Longitude(:,1),Latitude(:,1),EV_1km_RefSB(:,1,1),double(xq),double(yq),'cubic');
-%     mesh(xq,yq,z1)
+    [xq,yq] = meshgrid(min(Longitude(:,1)):.1:max(Longitude(:,1)), min(Latitude(:,1)):.1:max(Latitude(:,1)));
+    z1 = griddata(Longitude(:,1),Latitude(:,1),EV_1km_RefSB(:,1,1),double(xq),double(yq),'cubic');
+    contourf(xq,yq,z1)
+%     
+%     [xq1,yq1] = meshgrid(min(Longitude(1,:)):.2:max(Longitude(1,:)), min(Latitude(1,:)):.2:max(Latitude(1,:)));
+%     z2 = griddata(Longitude(1,:),Latitude(1,:),EV_1km_RefSB(1,:,1),double(xq1),double(yq1),'cubic');
+%     mesh(xq1,yq1,z2)
 
 % F = scatteredInterpolant(Longitude(:,1),Latitude(:,1),EV_1km_RefSB(:,1,1));
-% [xq,yq] = meshgrid(min(Longitude(:,1)):.5:max(Longitude(:,1)), min(Latitude(:,1)):.5:max(Latitude(:,1)));
-% F.Method = 'nearest';
+% [xq,yq] = meshgrid(min(Longitude(:,1)):.1:max(Longitude(:,1)), min(Latitude(:,1)):.1:max(Latitude(:,1)));
+% F.Method = 'linear';
 % vq1 = abs(F(xq,yq));
 % mesh(xq,yq,vq1)
+% 
+% F2 = scatteredInterpolant(Longitude(1,:)',Latitude(1,:)',EV_1km_RefSB(1,:,1)');
+% [xq1,yq1] = meshgrid(min(Longitude(1,:)):.1:max(Latitude(1,:)), min(Latitude(1,:)):.1:max(Latitude(1,:)));
+% F2.Method = 'linear';
+% vq2 = abs(F(xq1,yq1));
+% mesh(xq1,yq1,vq2)
+
 
 
     hold on
     plot3(Longitude(:,1),Latitude(:,1),EV_1km_RefSB(:,1,1),'r.')
-    hold on
-    plot3(Longitude(1,:),Latitude(1,:),EV_1km_RefSB(1,:,1),'b.')
+%     ylim([-40 40])
+%     hold on
+%     plot3(Longitude(1,:),Latitude(1,:),EV_1km_RefSB(1,:,1),'b.')
+%     ylim([-40 40])
   
     clear EV_1km_RefSB
     clear Longitude
