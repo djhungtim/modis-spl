1359961939f2a07a6370b911b51b6773bc13767f
Hong Hua Ting <djhungtim@gmail.com>
2020-04-30 23:59:04 +0800

    memory blow

--- one_for.m
+++ one_for.m
@@ -30,53 +30,59 @@ opengl('save', 'hardware')
 tic % start time tracker
 
 
-EV_RSB_dir = dir('D:\tim_spl\summerData\MOD021KM*.hdf');
+EV_RSB_dir = dir('D:\SPL\dataAnalysis\springData\MOD021KM*.hdf');
 
 
-RSB_NUM = 20;%length(EV_RSB_dir); % calculate length of RSB data
+RSB_NUM = length(EV_RSB_dir); % calculate length of RSB data
 
-EV_GEO_dir = dir('D:\tim_spl\summerDataGeo\MOD03*.hdf');
+EV_GEO_dir = dir('D:\SPL\dataAnalysis\springDataGeo\MOD03*.hdf');
 
 %import both Latitude and Longitude
-GEO_NUM = length(EV_GEO_dir); % calculate length of GEO data
+%GEO_NUM = length(EV_GEO_dir); % calculate length of GEO data
 
 
 %import EV_1km_RefSB filename
 
+along_track_length = 2030;
+along_scan_length = 1354;
 
-EV_1km_RefSB = zeros(2030,1354);
-Longitude = zeros(2030,1354);
-Latitude = zeros(2030,1354);
+
+% EV_1km_RefSB = zeros(along_track_length,along_scan_length);
+% Longitude = zeros(along_track_length,along_scan_length);
+% Latitude = zeros(along_track_length,along_scan_length);
 % along_track_Longitude = zeros(2030,1354);
 % along_track_Latitude = zeros(2030,1354);
 % along_track_EV_1km_RefSB = zeros(2030,1354);
 
+% grid_long = -180:1:180;
+% grid_lat = -90:1:90;
+grid_long = randi([-180,180],1,361);
+grid_lat = randi([-90,90],1,181);
+[meshlon, meshlat]=meshgrid(grid_long ,grid_lat);
+
+
 for idx = 1:RSB_NUM
     
     EV_1km_RefSB = hdfread([EV_RSB_dir(idx).folder,'\',EV_RSB_dir(idx).name],...
-        'MODIS_SWATH_Type_L1B', 'Fields', 'EV_1KM_RefSB', 'Index',{[1  1  1],[1  1  1],[1  2030  1354]});
+        'MODIS_SWATH_Type_L1B', 'Fields', 'EV_1KM_RefSB', 'Index',{[1  1  1],[1  1  1],[1  along_track_length  along_scan_length]});
         
-    EV_1km_RefSB(EV_1km_RefSB >= 65500 & EV_1km_RefSB <= 65535) = NaN;
-    EV_1km_RefSB(EV_1km_RefSB > 32767) = NaN;
+    
+
+    EV_1km_RefSB(EV_1km_RefSB == uint16(65535)) = 0;
+%     EV_1km_RefSB(EV_1km_RefSB == 0) = NaN;
+%     %EV_1km_RefSB(EV_1km_RefSB > 32767) = NaN;
     EV_1km_RefSB = double(EV_1km_RefSB);
     
     Longitude = hdfread([EV_GEO_dir(idx).folder,'\',EV_GEO_dir(idx).name],...
-        'MODIS_Swath_Type_GEO', 'Fields', 'Longitude', 'Index',{[1 1],[1 1],[2030 1354]});
-    Longitude(Longitude == -999) = NaN;
+        'MODIS_Swath_Type_GEO', 'Fields', 'Longitude', 'Index',{[1 1],[1 1],[along_track_length along_scan_length]});
+    Longitude(Longitude == single(-999)) = 0;
     Longitude = double(Longitude);
     
     Latitude = hdfread([EV_GEO_dir(idx).folder,'\',EV_GEO_dir(idx).name],...
-        'MODIS_Swath_Type_GEO', 'Fields', 'Latitude', 'Index',{[1 1],[1 1],[2030 1354]});
-    Latitude(Latitude == -999) = NaN;
+        'MODIS_Swath_Type_GEO', 'Fields', 'Latitude', 'Index',{[1 1],[1 1],[along_track_length along_scan_length]});
+    Latitude(Latitude == single(-999)) = 0;
     Latitude = double(Latitude);
-    
-%     [xq,yq] = meshgrid(min(Longitude(:,1)):.1:max(Longitude(:,1)), min(Latitude(:,1)):.1:max(Latitude(:,1)));
-%     z1 = griddata(Longitude(:,1),Latitude(:,1),EV_1km_RefSB(:,1,1),double(xq),double(yq),'cubic');
-%     contourf(xq,yq,z1)
-%     
-%     [xq1,yq1] = meshgrid(min(Longitude(1,:)):.2:max(Longitude(1,:)), min(Latitude(1,:)):.2:max(Latitude(1,:)));
-%     z2 = griddata(Longitude(1,:),Latitude(1,:),EV_1km_RefSB(1,:,1),double(xq1),double(yq1),'cubic');
-%     mesh(xq1,yq1,z2)
+   
 
 %     F = scatteredInterpolant(Longitude(:,1),Latitude(:,1),EV_1km_RefSB(:,1,1));
 %     [xq,yq] = meshgrid(min(Longitude(:,1)):.1:max(Longitude(:,1)), min(Latitude(:,1)):.1:max(Latitude(:,1)));
@@ -84,33 +90,68 @@ for idx = 1:RSB_NUM
 %     vq1 = abs(F(xq,yq));
 %     mesh(xq,yq,vq1)
 % 
-%     F2 = scatteredInterpolant(Longitude(1,:)',Latitude(1,:)',EV_1km_RefSB(1,:,1)');
-%     [xq1,yq1] = meshgrid(min(Longitude(1,:)):.1:max(Latitude(1,:)), min(Latitude(1,:)):.1:max(Latitude(1,:)));
-%     F2.Method = 'linear';
-%     vq2 = abs(F(xq1,yq1));
-%     mesh(xq1,yq1,vq2)
 
-    along_track_Longitude = Longitude(:,1);
-    along_track_Latitude = Latitude(:,1);
-    along_track_EV_1km_RefSB = EV_1km_RefSB(:,1,1);
+    along_track_Longitude = Longitude(1:2:along_track_length,1);
+    along_track_Latitude = Latitude(1:2:along_track_length,1);
+    along_track_EV_1km_RefSB = EV_1km_RefSB(1:2:along_track_length,1,1);
     
-    writematrix([along_track_Longitude,along_track_Latitude,along_track_EV_1km_RefSB],'along_data.csv')
+%     writematrix([along_track_Longitude,along_track_Latitude,along_track_EV_1km_RefSB],'along_data.csv')
     
+    along_scan_Longitude = Longitude(1,1:2:along_scan_length);
+    along_scan_Latitude = Latitude(1,1:2:along_scan_length);
+    along_scan_EV_1km_RefSB = EV_1km_RefSB(1,1:2:along_scan_length,1);
+    
+    All_Longitude = [along_track_Longitude;along_scan_Longitude'];
+    All_Latitude = [along_track_Latitude;along_scan_Latitude'];
+    All_EV_1km_RefSB = [along_track_EV_1km_RefSB;along_scan_EV_1km_RefSB'];
+    
+%     F = scatteredInterpolant(All_Longitude,All_Latitude,All_EV_1km_RefSB);
+%     F.Method = 'linear';
+%     EV_1km_RefSB_grid = F(meshlon,meshlat);
+%     contourf(EV_1km_RefSB_grid)
     
 
+    
+%     sort(All_Longitude)
+%     sort(All_Latitude)
+%     [xq,yq] = meshgrid(min(Longitude(:,1)):.1:max(Longitude(:,1)), min(Latitude(:,1)):.1:max(Latitude(:,1)));
+    
+    EV_1km_RefSB_grid = griddata(All_Longitude,All_Latitude,All_EV_1km_RefSB,meshlon,meshlat,'linear');
     hold on
-    plot3(along_track_Longitude,along_track_Latitude,along_track_EV_1km_RefSB,'r.')
+    contourf(EV_1km_RefSB_grid)
+    colormap jet
+    
+    load('D:\SPL\dataAnalysis\worldamap\hdcoast.mat');
+
+
+    set(gca,'XTick',0:60:360)
+    set(gca,'YTick',0:45:180)
+    set(gca,'xticklabel',{'180^oW','120^oW','60^oW','0^o','60^oE','120^oE','180^oE'});    
+    hold on
+    plot(hdcoast(:,1)+180,hdcoast(:,2)+90,'k','LineWidth',1)
+    set(gca,'yticklabel',{'90^oS','45^oS','0^o','45^oN','90^oN'});
+%     title('2018 September Equinox Earth Radiance from MODIS Terra');
+     
+%     contourf(xq,yq,z1)
+%     
+%     [xq1,yq1] = meshgrid(min(Longitude(1,:)):.2:max(Longitude(1,:)), min(Latitude(1,:)):.2:max(Latitude(1,:)));
+%     z2 = griddata(Longitude(1,:),Latitude(1,:),EV_1km_RefSB(1,:,1),double(xq1),double(yq1),'cubic');
+%     mesh(xq1,yq1,z2)
+
+%     hold on
+%     plot3(All_Longitude,All_Latitude,All_EV_1km_RefSB,'r.')
 %     ylim([-40 40])
-    hold on
-    plot3(Longitude(1,:),Latitude(1,:),EV_1km_RefSB(1,:,1),'b.')
+%     hold on
+%     plot3(along_scan_Longitude,along_scan_Latitude,along_scan_EV_1km_RefSB,'b.')
 %     ylim([-40 40])
 
 %     clear xq
 %     clear yq
 %   
-%     clear EV_1km_RefSB
-%     clear Longitude
-%     clear Latitude
+    clear EV_1km_RefSB
+    clear Longitude
+    clear Latitude
+    
 
 end
 
--- v1_0.m
+++ v1_0.m
@@ -1,4 +1,5 @@
- 
+id
+
 clear
 clc
 
